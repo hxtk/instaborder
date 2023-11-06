b8c7f9b721606b12c703ed197310071ab156a741
Peter Sanders <peter@psanders.me>
2023-11-05 22:01:14 -0500

    Control directory recursion behavior
    Add controls to determine whether/how the program will handle
    directories and hidden files that it encounters.
    
    We now ignore files names starting with "." by default, in line with
    default behavior on Linux and Unix operating systems, and only recurse
    into directories on request, to avoid inadvertently overwriting a
    large number of files.
    
    We now support multiple paths specified on the command line, as well,
    as the non-recursive use case would have been cumbersome if users could
    only specify a single file at a time.

--- src/instaborder/cli/_cli.py
+++ src/instaborder/cli/_cli.py
@@ -1,6 +1,9 @@
 import argparse
+import itertools
 import pathlib
+import sys
 import typing
+from typing import Iterable
 from typing import Iterator
 from typing import Optional
 
@@ -8,9 +11,11 @@ from PIL import Image as image  # noqa: N813 rename to match pep8 convention
 from PIL import ImageOps as image_ops  # noqa: N813
 
 
-def iter_files(dirpath: pathlib.Path) -> Iterator[pathlib.Path]:
+def iter_files(dirpath: pathlib.Path, all_files=False) -> Iterator[pathlib.Path]:
     try:
         for x in dirpath.iterdir():
+            if not all_files and x.name.startswith("."):
+                continue
             yield from iter_files(x)
     except NotADirectoryError:
         yield dirpath
@@ -22,9 +27,26 @@ def get_parser() -> argparse.ArgumentParser:
         description="Square pictures for instagram",
     )
     parser.add_argument(
-        "dir",
-        help="directory containing files to square",
+        "paths",
+        help="path to image or directory of images",
+        nargs="+",
         type=pathlib.Path,
+    )
+    parser.add_argument(
+        "--recursive",
+        "-r",
+        dest="recursive",
+        action="store_true",
+        default=False,
+        help="recurse into directories",
+    )
+    parser.add_argument(
+        "--all",
+        "-A",
+        dest="all_files",
+        action="store_true",
+        default=False,
+        help="include hidden files and directories"
     )
     return parser
 
@@ -49,16 +71,45 @@ def process(
 
 
 def main() -> None:
-    args = get_parser().parse_args()
-    directory = typing.cast(pathlib.Path, args.dir)
-    images = (
-        (x, image.open(x))
-        for x in iter_files(directory)
-        if x.suffix in (".jpg", ".jpeg")
+    parser = get_parser()
+    args = parser.parse_args()
+    paths: Iterable[pathlib.Path] = (
+        typing.cast(pathlib.Path, x) for x in args.paths
     )
-    images = ((path, process(img)) for path, img in images)
+
+    if args.recursive:
+        paths = itertools.chain.from_iterable((
+            iter_files(x, all_files=args.all_files)
+            for x in paths
+        ))
+    else:
+        paths, clone = itertools.tee(paths, 2)
+        for x in clone:
+            if x.is_dir():
+                print(
+                    f"Skipping directory: {x}",
+                    file=sys.stderr,
+                )
+    images: Iterable[tuple[pathlib.Path, image.Image]] = (
+        (x, image.open(x))
+        for x in paths
+        if x.is_file() and x.suffix in (".jpg", ".jpeg")
+    )
+
+    images, to_count = itertools.tee(images, 2)
+    found = sum(1 for _ in to_count)
+    print(f"{found} images found.", file=sys.stderr)
+    images = (
+        (path, process(img))
+        for path, img in images
+        if max(img.size) > min(img.size)
+    )
+
+    counter = 0
     for path, img in images:
         img.save(path)
+        counter += 1
+    print(f"{counter} images resized.", file=sys.stderr)
 
 
 if __name__ == "__main__":
